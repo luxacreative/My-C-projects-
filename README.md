06361947dd81d23076696b447816fb3f223cdd47
luxa_creative <147749167+luxacreative@users.noreply.github.com>
2025-07-23 17:06:15 +0300

    I abandoned the game.c MOVING TO OTHER THINGS

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # My-C-projects-
+
+##THE [GAME](https://github.com/luxacreative/My-C-projects-/blob/main/game.c) GAME PROABLY WONT WORK ON LINUX OR MAC OS
+
+
 i trying to learn C 
+## PLEASE FORK MY PROJECT AND EXTEND MY CODE ##
--- game.c
+++ game.c
@@ -1,3 +1,6 @@
+//ABANDOED, MOVED ON OTHER THINGS ( SEE IN THE REPO )
+
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -96,6 +99,20 @@ int startgame() {
     system("cls");
     printf("%s : %d/100 health \n", name, health);
     printf("It was a dark night, you, %s, were walking through the woods, when you suddenly heard a noise, it was a scream, a woman's scream, it camed from the abandoned house everyone was scared of, but the curiosity got the best of you, so you decided to go there. This is a mistake, maybe even your last one, but you dont know that yet.\n", name);
-    Sleep(5000);
+    Sleep(10000);
+    printf("As yoo opened the door, you see a bloddy knife on the floor, you pick it up, and suddently, you hear a noise from the kitchen\n");
+    printf("You slowly aprouch the kitchen door and open it.\n");
+    printf("There is a black cat there, it looks at you and runs away, you follow it, and it leads you to a room with a bed, a half open closet and a window.\n");
+    printf("You see a shadow from the closet, the closet door open by itself, and then, a 2 meter tall black creature comes out of it, and it looks at you with its red eyes, and then suddently, it jumpes at you, but you manage to dodge it, and run away, and the chassing begins:[PRESS W to run]\n");
+    Sleep(20000);
+    system("cls");
+    printf("%s : %d/100 health \n", name, health);
+    while (1) {
+        if (GetAsyncKeyState('W') & 0x8000) {
+            printf("You moved forward\n");
+            Sleep(200);
+        }}
+
+
 
 }
